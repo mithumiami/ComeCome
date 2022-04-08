71356f2a0bd8b60a987f3eee447fd32899d1d13d
mithumiami <mithumiami@gmail.com>
2022-04-08 13:02:31 +0000

    make Restaurant model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_02_002341) do
+ActiveRecord::Schema.define(version: 2022_04_08_125844) do
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.text "caption"
+    t.string "phone_number"
+    t.string "email"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
