3461690179564e67b97b9099ffffb0942c826a8e
mithumiami <mithumiami@gmail.com>
2022-04-15 14:53:34 +0000

    add devise admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-   root to: 'homes#top'
-   resources :users, only: [:show]
+  devise_for :admins, skip: [:passwords],controllers:{
+      sessions: 'admins/sessions'
+  }
+
+  namespace :admin do
+      get 'top' => 'homes#top', as: 'top'
+  end
+
+  devise_for :users, skip: [:passwords], controllers: {
+      sessions: 'devise/sessions',
+      registrations: 'devise/registrations'
+  }
+
+  scope module: :public do
+      root to: 'homes#top'
+      resources :users, only: [:show]
+
+  end
+
 end
